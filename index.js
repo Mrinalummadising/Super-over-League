b9b1909d8031a95db952ac6a3782044d00387052
Mrinalummadising <mrinalummadising>
2023-08-21 04:45:49 +0000

    SuperOver League

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+  <div className="container">
+    <h1>Super Over League</h1>
+    <div>
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
